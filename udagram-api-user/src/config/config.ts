08b8a7f443b7e1570ebbc27bf82494f265c34390
Duy Tô Quang <toduy96@gmail.com>
2024-10-11 01:40:45 +0700

    Part 1, 2, 3

---
+++ udagram-api-user/src/config/config.ts
@@ -0,0 +1,15 @@
+export const config = {
+  'username': process.env.POSTGRES_USERNAME,
+  'password': process.env.POSTGRES_PASSWORD,
+  'database': process.env.POSTGRES_DB,
+  'host': process.env.POSTGRES_HOST,
+  'dialect': 'postgres',
+  'aws_region': process.env.AWS_REGION,
+  'aws_profile': process.env.AWS_PROFILE,
+  'aws_media_bucket': process.env.AWS_BUCKET,
+  'aws_media_bucket_name': process.env.AWS_BUCKET_NAME,
+  'url': process.env.URL,
+  'jwt': {
+    'secret': process.env.JWT_SECRET,
+  },
+};
